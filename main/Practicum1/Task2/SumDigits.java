463d06beb9161a96d474206d51afb51896f85677
tinneua <roma.lugo@gmail.com>
2016-02-29 12:09:31 +0200

    task2

---
+++ main/Practicum1/Task2/SumDigits.java
@@ -0,0 +1,21 @@
+package main.Practicum1.Task2;
+
+/**
+ * Обчислити суму цифр числа.
+ Наприклад сума цифр числа 123 дорівнює  6.
+ */
+
+public class SumDigits {
+    public static void main(String[] args) {
+    int num = 123;
+        System.out.println(new SumDigits().sum(num));
+    }
+    public int sum(int number) {
+        int sum = 0;
+        while (number !=0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return Math.abs(sum);
+    }
+}
